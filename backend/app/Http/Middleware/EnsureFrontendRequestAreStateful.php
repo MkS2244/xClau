65f393d307aac7e15c5438eea2beb2fb83f8fd62
MkS2244 <13161523@alu.murciaeduca.es>
2025-05-14 19:53:45 +0200

    configuración de Dockerfiles, .env y docker-compose.yml

---
+++ backend/app/Http/Middleware/EnsureFrontendRequestAreStateful.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureFrontendRequestAreStateful
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+
+    /* 
+     *
+     * This middleware is used to ensure that frontend requests are stateful.
+     * It checks if the request is coming from a frontend application and
+     * applies the necessary stateful authentication.
+     */
+    protected $middlewareGroups = [
+        'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+        // ...
+    ];
+}
